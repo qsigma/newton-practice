aa30a05feb8911d7658955c2d401c1a98f0d6bde
Xinqi <qxq@berkeley.edu>
2024-08-22 13:51:41 -0700

    fixed: eps can be adjusted

--- newton.py
+++ newton.py
@@ -13,9 +13,9 @@ def second_order_deriv(x, eps=1e-4):
     """Calculate the second derivative."""
     if not eps > 0:
         eps = 1e-4
-    return (first_order_deriv(x+eps)-first_order_deriv(x))/eps
+    return (first_order_deriv(x+eps, eps)-first_order_deriv(x, eps))/eps
 
-def newton(x, threshold=0.01):
+def newton(x, eps=1e-4, threshold=0.01):
     """
     Do newton iterations, until convergence.
 
@@ -23,14 +23,16 @@ def newton(x, threshold=0.01):
     x -- the initial position
     threshold -- the threshold for the changing value to be small enough to terminate the iteration
     """
+    if not eps > 0:
+        eps = 1e-4
     if not threshold > 0:
         threshold = 0.01
-    first_deriv = first_order_deriv(x)
-    second_deriv = second_order_deriv(x)
+    first_deriv = first_order_deriv(x, eps)
+    second_deriv = second_order_deriv(x, eps)
     while abs(first_deriv/second_deriv) > threshold:
         x = x - first_deriv/second_deriv
-        first_deriv = first_order_deriv(x)
-        second_deriv = second_order_deriv(x)
+        first_deriv = first_order_deriv(x, eps)
+        second_deriv = second_order_deriv(x, eps)
     return x
 
 if __name__ == "__main__":
